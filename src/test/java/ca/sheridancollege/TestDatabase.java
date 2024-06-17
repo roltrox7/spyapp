a2200f27cd3cccc6262afdd1dbd43d5fb10e2024
roltrox7 <107786410+roltrox7@users.noreply.github.com>
2024-06-17 07:37:18 +0530

    Update TestDatabase.java

--- src/test/java/ca/sheridancollege/TestDatabase.java
+++ src/test/java/ca/sheridancollege/TestDatabase.java
@@ -19,84 +19,100 @@ import ca.sheridancollege.database.DatabaseAccess;
 @SpringBootTest
 class TestDatabase {
 
-	private DatabaseAccess da;
-	
-	@Autowired
-	public void setDa(DatabaseAccess da) {
-		this.da = da;
-	}
+    private DatabaseAccess da;
+    
+    @Autowired
+    public void setDa(DatabaseAccess da) {
+        this.da = da;
+    }
 
-	@Test
-	public void testDatabaseAddMission() {
-		// it tests add functionality on Database directly
-		Mission mission = new Mission();
-		mission.setAgent("Starlord");
-		mission.setGadget1("The element gun");
-		mission.setGadget2("Sub-machine gun");
-		mission.setTitle("Peter Quill");
+    @Test
+    public void testDatabaseAddMission() {
+        // it tests add functionality on Database directly
+        Mission mission = new Mission();
+        mission.setAgent("Starlord");
+        mission.setGadget1("The element gun");
+        mission.setGadget2("Sub-machine gun");
+        mission.setTitle("Peter Quill");
 
-		// get the original number of Missions
-		int origSize = da.getMissions("Starlord").size();
+        // get the original number of Missions
+        int origSize = da.getMissions("Starlord").size();
 
-		da.addMission(mission);
-		// the size after adding a mission
-		int newSize = da.getMissions("Starlord").size();
+        da.addMission(mission);
+        // the size after adding a mission
+        int newSize = da.getMissions("Starlord").size();
 
-		// it will pass if the new size is 1 bigger than the original
-		assertEquals(newSize, origSize + 1);
-	}
+        // it will pass if the new size is 1 bigger than the original
+        assertEquals(newSize, origSize + 1);
+    }
 
-	@Test
-	public void testDatabaseDeteteMission() {
-		// retrieves the mission
-		List<Mission> missions = da.getMissions("Johnny English");
+    @Test
+    public void testDatabaseDeleteMission() {
+        // retrieves the mission
+        List<Mission> missions = da.getMissions("Johnny English");
 
-		// get an id
-		Long id = missions.get(0).getId();
+        // Check if there are missions to delete
+        assertFalse(missions.isEmpty(), "No missions found for Johnny English");
 
-		// get the size of he original missions
-		int origSize = da.getMissions(missions.get(0).getAgent()).size();
+        // get an id
+        Long id = missions.get(0).getId();
 
-		da.deleteMission(id);
-		// the size after deleting a mission
-		int newSize = da.getMissions(missions.get(0).getAgent()).size();
+        // get the size of the original missions
+        int origSize = da.getMissions(missions.get(0).getAgent()).size();
 
-		// it will pass if the new size is 1 smaller than the original
-		assertEquals(newSize, origSize - 1);
-	}
+        da.deleteMission(id);
+        // the size after deleting a mission
+        int newSize = da.getMissions(missions.get(0).getAgent()).size();
 
-	@Test
-	public void testDatabaseUpdateMission() {
-		// retrieves the mission and fields
-		List<Mission> missions = da.getMissions("Johnny English");		
-		Mission mission = missions.get(0);
-		Long id = mission.getId();
-		
-		// set the title of the mission
-		mission.setTitle("Rescue the world");	
+        // it will pass if the new size is 1 smaller than the original
+        assertEquals(newSize, origSize - 1);
+    }
 
-		mission = da.getMission(id);
-		
-		// pass if the title equals to what the user tested
-		assertEquals(mission.getTitle(), "Rescue the world");
-	}
-	
-	@Test
-	public void testDatabaseGetMission() {
-		// retrieves the mission and field
-		List<Mission> missions = da.getMissions("Johnny English");		
-		Mission mission = da.getMission(missions.get(0).getId());
-		
-		// compare the title retrieved 
-		assertEquals(mission.getTitle(), "Rescue the Queen");
-	}
+    @Test
+    public void testDatabaseUpdateMission() {
+        // retrieves the mission and fields
+        List<Mission> missions = da.getMissions("Johnny English");
 
-	@Test
-	public void testDatabaseGetMissions() {
-		// retrieves the mission and field
-		List<Mission> missions = da.getMissions("Johnny English");
-		Mission mission = missions.get(0);
-		
-		assertEquals(mission.getTitle(), "Rescue the Queen");
-	}
+        // Check if there are missions to update
+        assertFalse(missions.isEmpty(), "No missions found for Johnny English");
+
+        Mission mission = missions.get(0);
+        Long id = mission.getId();
+        
+        // set the title of the mission
+        mission.setTitle("Rescue the world");
+        da.updateMission(mission); // Assuming there's a method to update the mission
+
+        mission = da.getMission(id);
+        
+        // pass if the title equals to what the user tested
+        assertEquals(mission.getTitle(), "Rescue the world");
+    }
+    
+    @Test
+    public void testDatabaseGetMission() {
+        // retrieves the mission and field
+        List<Mission> missions = da.getMissions("Johnny English");
+
+        // Check if there are missions to get
+        assertFalse(missions.isEmpty(), "No missions found for Johnny English");
+
+        Mission mission = da.getMission(missions.get(0).getId());
+        
+        // compare the title retrieved 
+        assertEquals(mission.getTitle(), "Rescue the Queen");
+    }
+
+    @Test
+    public void testDatabaseGetMissions() {
+        // retrieves the mission and field
+        List<Mission> missions = da.getMissions("Johnny English");
+
+        // Check if there are missions to get
+        assertFalse(missions.isEmpty(), "No missions found for Johnny English");
+
+        Mission mission = missions.get(0);
+        
+        assertEquals(mission.getTitle(), "Rescue the Queen");
+    }
 }
