07b0a1eaa45d223cc037e032fb6e5b5a92597894
roltrox7 <107786410+roltrox7@users.noreply.github.com>
2024-07-13 10:55:47 +0530

    add values to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,18 @@
 INSERT INTO missions
     (agent, title, gadget1, gadget2)
 VALUES
-    ('Johnny English', 'Rescue the Queen',
-    'Exploding Cigar', 'Voice Controlled Rolls Royce'),
-    
-    ('Natasha Romanova', 'Kill Iron Man',
-    'Armored Suit', 'Indestructible Pole');
+    ('Johnny English', 'Rescue the Queen', 'Exploding Cigar', 'Voice Controlled Rolls Royce'),
+    ('Natasha Romanova', 'Kill Iron Man', 'Armored Suit', 'Indestructible Pole'),
+    ('James Bond', 'Retrieve Stolen Microchip', 'Omega Seamaster Watch', 'Aston Martin DB5'),
+    ('Ethan Hunt', 'Infiltrate the Kremlin', 'Face Maker', 'Grapple Gun'),
+    ('Jason Bourne', 'Uncover Conspiracy', 'Encrypted Phone', 'Combat Knife'),
+    ('Austin Powers', 'Defeat Dr. Evil', 'Miniature Camera', 'Shaguar'),
+    ('Lara Croft', 'Find the Lost Tomb', 'Dual Pistols', 'Climbing Gear'),
+    ('Indiana Jones', 'Recover the Holy Grail', 'Whip', 'Revolver'),
+    ('Catwoman', 'Steal the Diamond', 'Claws', 'Motorcycle'),
+    ('Jack Reacher', 'Expose Corruption', 'Bulletproof Vest', 'Sniper Rifle'),
+    ('Harry Hart', 'Stop Valentine', 'Umbrella Shield', 'Lighter Grenade'),
+    ('Maxwell Smart', 'Thwart KAOS', 'Shoe Phone', 'Cone of Silence'),
+    ('Nick Fury', 'Assemble the Avengers', 'Eye Patch', 'Jet Pack'),
+    ('Dom Cobb', 'Perform Inception', 'Totem', 'Dream Machine'),
+    ('Sydney Bristow', 'Take Down SD-6', 'Lipstick Gun', 'EMP Device');
