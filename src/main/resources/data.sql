b723a0957a1891d595a4dea0c4a8caa93af90328
roltrox7 <107786410+roltrox7@users.noreply.github.com>
2024-07-13 11:49:01 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,9 @@ INSERT INTO missions
 VALUES
     ('Johnny English', 'Rescue the Queen',
     'Exploding Cigar', 'Voice Controlled Rolls Royce'),
+
+     ('Austin Powers', 'Rescue the Prime Minister',
+    'Exploding Cigar', 'Jetpack'),
     
     ('Natasha Romanova', 'Kill Iron Man',
     'Armored Suit', 'Indestructible Pole');
