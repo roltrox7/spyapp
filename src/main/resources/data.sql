d63aeb31dccdf27aa612996f0cd1d8f750ccf4b2
roltrox7 <saathviksshetty@gmail.com>
2024-07-14 09:53:47 +0530

    change data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,11 @@
 INSERT INTO missions
     (agent, title, gadget1, gadget2)
 VALUES
-    -- Johnny English missions
-    ('Johnny English', 'Rescue the Queen', 'Exploding Cigar', 'Voice Controlled Rolls Royce'),
-    ('Johnny English', 'Infiltrate Enemy Base', 'Invisible Cloak', 'Laser Pen'),
-    ('Johnny English', 'Protect the Prime Minister', 'Rocket Shoes', 'X-ray Glasses'),
+    ('Johnny English', 'Rescue the Queen',
+    'Exploding Cigar', 'Voice Controlled Rolls Royce'),
 
-    -- Austin Powers missions
-    ('Austin Powers', 'Rescue the Boss', 'Exploding Cigar', 'Voice Controlled Rolls Royce'),
-    ('Austin Powers', 'Defeat Dr. Evil', 'Mini Submarine', 'Magnetic Watch'),
-    ('Austin Powers', 'Retrieve Stolen Mojo', 'Charm Ray', 'Spy Car'),
-
-    -- Natasha Romanova missions
-    ('Natasha Romanova', 'Kill Iron Man', 'Armored Suit', 'Indestructible Pole'),
-    ('Natasha Romanova', 'Retrieve Secret Data', 'Holographic Disguise', 'Electrified Gloves'),
-    ('Natasha Romanova', 'Infiltrate Hydra', 'Grappling Hook', 'Nanotech Suit');
+    ('Austin Powers', 'Rescue the Boss',
+    'Exploding Cigar', 'Voice Controlled Rolls Royce')
+    
+    ('Natasha Romanova', 'Kill Iron Man',
+    'Armored Suit', 'Indestructible Pole');
